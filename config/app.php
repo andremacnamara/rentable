f3b201b9ec83b5243301d1809c51b5a469d9b61a
Andre MacNamara <Andremacnamara12@gmail.com>
2018-05-05 12:10:55 +0100

    Failed Chart Attempt

--- config/app.php
+++ config/app.php
@@ -146,7 +146,7 @@ return [
         Illuminate\Translation\TranslationServiceProvider::class,
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
-        ConsoleTVs\Charts\ChartsServiceProvider::class,
+       
 
         /*
          * Package Service Providers...
@@ -209,7 +209,6 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
-
     ],
 
 ];
