2208b0f93169a1cfe19087a9a78b4d768e0eb5e2
Andre MacNamara <Andremacnamara12@gmail.com>
2018-05-22 17:19:13 +0100

    Fixed App Servce Pro for deploy

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -6,17 +6,13 @@ use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\Schema;
 
 class AppServiceProvider extends ServiceProvider
-{
-    /**
-     * Bootstrap any application services.
-     *
-     * @return void
-     */
-    public function boot()
+{public function boot(UrlGenerator $url)
     {
-        Schema::defaultStringLength(191);
+        if(env('REDIRECT_HTTPS')) {
+            $url->formatScheme('https');
+        }
     }
-
+ 
     /**
      * Register any application services.
      *
@@ -24,6 +20,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        //
+        if(env('REDIRECT_HTTPS')) {
+            $this->app['request']->server->set('HTTPS', true);
+        }
     }
 }
